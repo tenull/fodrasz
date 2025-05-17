69c09f650cdfad299a88f626c2dfaab5b7de8eda
Tamas  Nagy <tamas.nagy@vermis.hu>
2025-05-17 14:25:48 +0200

    Clean initial commit2

--- backend/server.js
+++ backend/server.js
@@ -27,4 +27,4 @@ app.post('/proxy', async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => console.log('Proxy szerver fut a 3001-es porton'));
+app.listen(process.env.PORT, () => console.log(`Proxy szerver fut a ${process.env.PORT} -es porton`));
